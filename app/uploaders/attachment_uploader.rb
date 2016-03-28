88924d66415ab0fa8adc1b3b4ee7d8435ba30eeb
chuongtrinh <ctrinh@tamu.edu>
2016-03-28 18:19:45 +0000

    Completed uploading sheet page with restriction on file type

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -14,6 +14,10 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end  
+  
+  def extension_white_list
+      %w(xlsx xls csv)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
